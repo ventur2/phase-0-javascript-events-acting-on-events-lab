de09c5948952a5f9356f516999b0586346404dbb
rachell <dazzlemevanity@gmail.com>
2022-06-13 12:00:15 -0400

    commit tree c8183d89eb48cf28aa13e03c13c28fdbdf6d1751

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Your code here
+// document.addEventListener("keydown", function (e) {
+//     if (e.key === "ArrowLeft") {
+//       moveDodgerLeft();
+//     }
+//   });
+ 
+  function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  
+  function moveDodgerRight() {
+      const leftNumbers = dodger.style.left.replace ("px", "");
+      const left = parseInt(leftNumbers, 10);
+    
+      if (left > 0) {
+        dodger.style.left = `${left + 1}px`;
+      }
+  }
